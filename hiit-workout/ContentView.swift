86a73360e0a7c6de54213d5e0448b836ef20030e
Mitchel <mwassler@covermymeds.com>
2020-05-18 16:29:09 -0400

    Initial Commit

---
+++ hiit-workout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hiit-workout
+//
+//  Created by Mitchel Wassler on 5/18/20.
+//  Copyright © 2020 Mitchel Wassler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
