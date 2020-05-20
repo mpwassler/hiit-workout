09a064b5e8974d1b19f05a2f4312943768eacc69
Mitchel <mwassler@covermymeds.com>
2020-05-19 23:03:44 -0400

    Fix typo in test

--- hiit-workoutTests/models/CountDownTests.swift
+++ hiit-workoutTests/models/CountDownTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class CountDownTests: XCTestCase {
     
-    var sut : Clock!
+    var sut : CountDown!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
